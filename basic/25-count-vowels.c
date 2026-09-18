0608740163d26866249253e341fa2f52c0cf9892
Pedro Fonseca <contatopedrorn@gmail.com>
2026-09-18 17:53:44 -0300

    add count vowels

--- basic/25-count-vowels.c
+++ basic/25-count-vowels.c
@@ -2,9 +2,21 @@
 
 int main()
 {
+    char vowels[5] = {'a', 'e', 'i', 'o', 'u'};
     char text[100];
     printf("Enter a word or sentence: ");
     fgets(text, sizeof(text), stdin);
+    int count = 0; 
+    for (int i = 0; i < sizeof(text); i++)
+    {
+        for (int j = 0; j < sizeof(vowels); j++) {
+            if (text[i] == vowels[j]) {
+                count++;
+            }
+        }
+    }
+
+    printf("There are %d vowels\n", count);
 
     return 0;
 }
