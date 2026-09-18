bc5d55f2c372b5b38b58036686f2d1d956bd16a6
Pedro Fonseca <contatopedrorn@gmail.com>
2026-09-18 16:59:51 -0300

    add fibonacci sequence

--- basic/21-fibonacci-sequence.c
+++ basic/21-fibonacci-sequence.c
@@ -6,5 +6,33 @@ int main()
     printf("Enter N: ");
     scanf("%d", &n);
 
+    int next;
+    int back=0;
+    int current=1;
+    for (int i = 0; i <= n; i++)
+    {
+        if (i == 0)
+        {
+            // first interaction -> 0 (i = 0)
+            printf("%d ", i);
+        }
+        else if (i == 1)
+        {
+            // second interaction -> 1 (i = 1)
+            printf("%d ", i);
+        }
+        else
+        {
+            next = back + current;
+            printf("%d ", next);
+
+            back = current;
+            current = next;
+        }
+    }
+    printf("\n");
     return 0;
 }
+//  0 -> i + (i -1) -> 0 + (0 - 1) = not work > i = 0
+//  1 -> 1 + (1 - 1) -> 1 = 1
+//  2 -> 2 + (2 - 1) ->
